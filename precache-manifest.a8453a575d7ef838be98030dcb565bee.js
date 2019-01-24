08ed15357006c99a21d6149ca24a76ad1e88ea56
Ian Gornall <ian.gornall@gmail.com>
2019-01-24 18:37:34 -0500

    Updates

---
+++ precache-manifest.a8453a575d7ef838be98030dcb565bee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c3ac8d7e7b2c177719f90224f25a15e",
+    "url": "/memoria/static/media/persistence-of-memory.1c3ac8d7.jpg"
+  },
+  {
+    "revision": "44dc571d43b172cf83fd",
+    "url": "/memoria/static/js/runtime~main.44dc571d.js"
+  },
+  {
+    "revision": "16c1420f930eac353ef1",
+    "url": "/memoria/static/js/main.16c1420f.chunk.js"
+  },
+  {
+    "revision": "c18ce592240245e16efd",
+    "url": "/memoria/static/js/1.c18ce592.chunk.js"
+  },
+  {
+    "revision": "16c1420f930eac353ef1",
+    "url": "/memoria/static/css/main.e0d5f29e.chunk.css"
+  },
+  {
+    "revision": "208ad21a4f502db72ffc5c3814064c2d",
+    "url": "/memoria/index.html"
+  }
+];
